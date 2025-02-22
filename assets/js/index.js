c4b875454e3a6f39f623406bbc2423de9119b4fa
Hoàng <chunghoang26826@gmail.com>
2025-02-22 11:40:22 +0700

    btn moble

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,22 +8,22 @@ const form2 = document.getElementById('sign-in_form');
 const form3 = document.getElementById('forgot-pass_form');
 
 signin.addEventListener("click", () => {
-    form2.style.display = "flex";
     form2.style.animation = 'move 0.5s ease-in-out';
+    form2.style.display = "flex";
     form1.style.display = "none";
 });
 signup.addEventListener("click", () => {
-    form1.style.display = "flex";
     form1.style.animation = 'move 0.5s ease-in-out';
+    form1.style.display = "flex";
     form2.style.display = "none";
 });
 forgot.addEventListener("click", () => {
-    form3.style.display = "flex";
     form3.style.animation = 'move 0.5s ease-in-out';
+    form3.style.display = "flex";
     form2.style.display = "none";
 });
 signin1.addEventListener("click", () => {
-    form2.style.display = "flex";
     form2.style.animation = 'move 0.5s ease-in-out';
+    form2.style.display = "flex";
     form3.style.display = "none";
 });
