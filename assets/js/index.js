1d0df4bddc38a79f7effd49588acc81d87697c96
Hoàng <chunghoang26826@gmail.com>
2025-02-22 20:54:44 +0700

    up js

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,3 +27,73 @@ signin1.addEventListener("click", () => {
     form2.style.display = "flex";
     form3.style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("sign-up_form");
+    const inputs = form.querySelectorAll("input, textarea, select");
+    const submitButton = document.getElementById("sign-up_btn");
+
+    function checkInputs() {
+        let allFilled = true;
+        inputs.forEach(input => {
+            if (input.value.trim() === "") {
+                allFilled = false;
+            }
+        });
+        submitButton.disabled = !allFilled;
+        submitButton.style.color = allFilled ? '#3983d6' : '#3e3e3e';
+        submitButton.style.background = allFilled ? '#ffffff' : '#c2c2c2';
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener("input", checkInputs);
+    });
+
+    checkInputs();
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("sign-in_form");
+    const inputs = form.querySelectorAll("input, textarea, select");
+    const submitButton = document.getElementById("sign-in_btn");
+
+    function checkInputs() {
+        let allFilled = true;
+        inputs.forEach(input => {
+            if (input.value.trim() === "") {
+                allFilled = false;
+            }
+        });
+        submitButton.disabled = !allFilled;
+        submitButton.style.color = allFilled ? '#13ca91' : '#3e3e3e';
+        submitButton.style.background = allFilled ? '#ffffff' : '#c2c2c2';
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener("input", checkInputs);
+    });
+
+    checkInputs();
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("forgot-pass_form");
+    const inputs = form.querySelectorAll("input, textarea, select");
+    const submitButton = document.getElementById("forgot_btn");
+
+    function checkInputs() {
+        let allFilled = true;
+        inputs.forEach(input => {
+            if (input.value.trim() === "") {
+                allFilled = false;
+            }
+        });
+        submitButton.disabled = !allFilled;
+        submitButton.style.color = allFilled ? '#13ca91' : '#3e3e3e';
+        submitButton.style.background = allFilled ? '#ffffff' : '#c2c2c2';
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener("input", checkInputs);
+    });
+
+    checkInputs();
+});
